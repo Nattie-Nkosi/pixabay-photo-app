d56bd0ae5af991ca90d26f3ac5113508824f7a9a
Nutty4616 <nkosin361@gmail.com>
2022-05-13 00:26:54 +0200

    Styling SearchBar Component, making network request to pixabay

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
 
-const App = () => {
-  return <div>App Component</div>;
-};
+/* This is a Parent Component */
+class App extends Component {
+  onSearchSubmit(term) {
+    axios.get("https://pixabay.com/api/", {
+      params: {
+        key: "27369439-109e49024edf38b4bfd684f4c",
+        q: term,
+        lang: "en",
+        image_type: "all",
+        orientation: "all",
+        order: "popular",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
