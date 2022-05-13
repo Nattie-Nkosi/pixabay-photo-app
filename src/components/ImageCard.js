063670e05d9ef5e77d3eb921fef39b45028dd736
Nutty4616 <nkosin361@gmail.com>
2022-05-13 13:59:00 +0200

    Creating ImageCard component

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 
+/* 
+  TODO: 
+  TODO: 
+*/
+
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  // componentDidMount is will be called after the component is rendered
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
   render() {
     const { tags, largeImageURL } = this.props.image;
 
     return (
-      <div>
-        <img alt={tags} src={largeImageURL} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={tags} src={largeImageURL} />
       </div>
     );
   }
